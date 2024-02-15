aa21df3fd2feee8833ff48cf6cf117b31cc18b05
Victor Oliveira <vhdestroy@hotmail.com>
2024-02-15 11:09:20 -0300

    🌈 style: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    font-size: 62.5%;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-size: 1.6rem;
+    color: ${props => props.theme['gray-300']};
+
+    background-color: ${props => props.theme['gray-900']};
+  }
+
+  body, input, textarea, button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+  }
+`;
